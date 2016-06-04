b9119a489a7c1bdd9ab445fffc33d75da39ef5ea
Sheikh Araf <arafsheikh@rocketmail.com>
2016-06-04 22:35:06 +0530

    core: Refactor for Gitmate

--- com.coala.core/src/com/coala/core/handlers/RemoveMarkers.java
+++ com.coala.core/src/com/coala/core/handlers/RemoveMarkers.java
@@ -1,4 +1,3 @@
-
 package com.coala.core.handlers;
 
 import org.eclipse.core.commands.AbstractHandler;
@@ -13,33 +12,37 @@ import org.eclipse.ui.PlatformUI;
 
 public class RemoveMarkers extends AbstractHandler {
 
-    @Override
-    public Object execute(ExecutionEvent event) throws ExecutionException {
-        IFile file = ((IFileEditorInput) PlatformUI.getWorkbench()
-                .getActiveWorkbenchWindow().getActivePage().getActiveEditor()
-                .getEditorInput()).getFile();
-        removeAllMarkers(file);
-        return null;
-    }
+  @Override
+  public Object execute(ExecutionEvent event) throws ExecutionException {
+    IFile file = ((IFileEditorInput) PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+        .getActivePage().getActiveEditor().getEditorInput()).getFile();
+    removeAllMarkers(file);
+    return null;
+  }
 
-    public void removeAllMarkers(IFile file) {
-        IMarker[] problems = null;
-        IResource resource = (IResource) file;
-        int depth = IResource.DEPTH_INFINITE;
-        try {
-            problems = resource.findMarkers(IMarker.PROBLEM, true, depth);
+  /**
+   * Remove all markers of the type "com.coala.core.coolproblem" from the given file.
+   * @param file IFile to remove markers from
+   */
+  public void removeAllMarkers(IFile file) {
+    IMarker[] problems = null;
+    IResource resource = (IResource) file;
+    int depth = IResource.DEPTH_INFINITE;
+    try {
+      problems = resource.findMarkers(IMarker.PROBLEM, true, depth);
 
-        } catch (CoreException e) {
-            e.printStackTrace();
-        }
-        for (IMarker m : problems) {
-            try {
-                if (m.getType().equals("com.coala.core.coolproblem"))
-                    m.delete();
-            } catch (CoreException e) {
-                e.printStackTrace();
-            }
-        }
+    } catch (CoreException ex) {
+      ex.printStackTrace();
     }
+    for (IMarker m : problems) {
+      try {
+        if (m.getType().equals("com.coala.core.coolproblem")) {
+          m.delete();
+        }
+      } catch (CoreException ex) {
+        ex.printStackTrace();
+      }
+    }
+  }
 
 }
